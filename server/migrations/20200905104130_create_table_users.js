513bf4dea8fd1d81368183530d918794b50c70b0
camposyuri <yurinapoleao@gmail.com>
2020-09-05 11:09:15 -0300

    [ADD] - Create table users

--- server/migrations/20200905104130_create_table_users.js
+++ server/migrations/20200905104130_create_table_users.js
@@ -1,8 +1,12 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNull();
+    table.string("email").notNull().unique();
+    table.boolean("admin").notNull().defaultTo(false);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.dropTable("users");
 };
