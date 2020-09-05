a81a7d7dc55a348c681ca4910cfed8452829e0de
camposyuri <yurinapoleao@gmail.com>
2020-09-05 13:09:25 -0300

    [ADD] - Create routes api and logic of http methods

--- server/api/user.js
+++ server/api/user.js
@@ -1,7 +1,89 @@
 module.exports = (app) => {
+  const { existsOrError } = app.api.validation;
+
   const save = (req, res) => {
-    res.send("user save");
+    const user = { ...req.body };
+
+    if (req.params.id) {
+      user.id = req.params.id;
+    }
+
+    try {
+      existsOrError(user.name, "Name not informed.");
+      existsOrError(user.email, "Email not informed.");
+      // existsOrError(user.admin, "Admin not informed.");
+    } catch (msg) {
+      res.status(400).send(msg);
+    }
+
+    if (user.id) {
+      app
+        .db("users")
+        .update(user)
+        .where({ id: user.id })
+        .then((_) => res.status(204).send())
+        .catch((err) => res.status(500).send(err));
+    } else {
+      app
+        .db("users")
+        .insert(user)
+        .then((_) => res.status(204).send())
+        .catch((err) => res.status(500).send(err));
+    }
   };
 
-  return { save };
+  const remove = async (req, res) => {
+    try {
+      const rowsDeleted = await app
+        .db("users")
+        .where({ id: req.params.id })
+        .del();
+
+      try {
+        existsOrError(rowsDeleted, "User not found.");
+      } catch (msg) {
+        return res.status(400).send(msg);
+      }
+
+      res.status(204).send();
+    } catch (msg) {
+      res.status(500).send(msg);
+    }
+  };
+
+  const limit = 10;
+  const get = async (req, res) => {
+    const page = req.query.page || 1;
+
+    const result = await app.db("users").count("id").first();
+    const count = parseInt(result.count);
+
+    app
+      .db("users")
+      .select("id", "name", "email", "admin")
+      .orderBy("id")
+      .limit(limit)
+      .offset(page * limit - limit)
+      .then((users) =>
+        res.json({
+          data: users,
+          count,
+          limit,
+        })
+      )
+      .catch((err) => res.status(500).send(err));
+  };
+
+  const getById = (req, res) => {
+    app
+      .db("users")
+      .where({ id: req.params.id })
+      .first()
+      .then((user) => {
+        return res.json(user);
+      })
+      .catch((err) => res.status(500).send(err));
+  };
+
+  return { save, remove, get, getById };
 };
